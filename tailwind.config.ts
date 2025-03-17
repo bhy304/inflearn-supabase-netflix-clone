43073abebe603ff11913471aa205e2e10d9b8192
Hayeon Baek <bhy0512@gmail.com>
2025-03-17 15:52:13 +0900

    Chore: Initialize Netflix clone project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import withMT from "@material-tailwind/react/utils/withMT";
 
 const config: Config = {
   content: [
@@ -15,6 +16,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require('@tailwindcss/typography')],
+}
+export default withMT(config)
