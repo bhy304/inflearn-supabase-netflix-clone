92b2e3292fc99735a7ab4da0e3ed89b5da18d74d
Hayeon Baek <bhy0512@gmail.com>
2025-03-21 16:44:42 +0900

    Feat: Implement infinite scroll feature and perform SEO work for better search

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,7 +1,32 @@
+import { Metadata, ResolvingMetadata } from 'next'
 import UI from './ui'
 import { getMovie } from '@/actions/movie-actions'
 
-export default async function MovieDetail({ params: { id } }) {
+type Props = {
+  params: { id: number }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata({
+  params,
+  searchParams,
+}: Props): Promise<Metadata> {
+  const movie = await getMovie(params.id)
+
+  return {
+    title: movie?.title,
+    description: movie?.overview,
+    openGraph: {
+      images: [movie?.image_url ?? ''],
+    },
+  }
+}
+
+export default async function MovieDetail({
+  params: { id },
+}: {
+  params: { id: number }
+}) {
   const movie = await getMovie(id)
 
   return (
--- components/movie-card.tsx
+++ components/movie-card.tsx
@@ -8,7 +8,7 @@ export default function MovieCard({ movie: { id, title, image_url } }) {
       <img src={image_url} alt={title} className='w-full' />
       <Link href={`/movies/${id}`}>
         <div className='absolute flex items-center justify-center top-0 bottom-0 left-0 right-0 z-10 bg-black  opacity-0 hover:opacity-80 transition-opacity duration-300'>
-          <p className='text-xl text-white'>{title}</p>
+          <p className='text-xl text-white text-center'>{title}</p>
         </div>
       </Link>
     </div>
